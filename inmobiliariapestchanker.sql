ee570292b54d24ce634e770bf8b87b72f2043bd6
fpest <federico@ncdsanluis.com.ar>
2022-09-06 17:51:06 -0300

    Update inmobiliariapestchanker.sql

--- inmobiliariapestchanker.sql
+++ inmobiliariapestchanker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-08-2022 a las 03:57:14
+-- Tiempo de generación: 06-09-2022 a las 22:47:58
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.1
 
@@ -36,6 +36,14 @@ CREATE TABLE `contrato` (
   `Precio` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `contrato`
+--
+
+INSERT INTO `contrato` (`Id`, `IdInmueble`, `IdInquilino`, `FechaInicio`, `FechaFin`, `Precio`) VALUES
+(5, 7, 7, '2022-09-06', '2022-10-02', '9999'),
+(6, 5, 6, '2022-10-02', '2022-12-04', '8888');
+
 -- --------------------------------------------------------
 
 --
@@ -46,14 +54,22 @@ CREATE TABLE `inmueble` (
   `Id` int(11) NOT NULL,
   `IdPropietario` int(11) NOT NULL,
   `Direccion` varchar(100) NOT NULL,
-  `CoordenadaN` varchar(30) NOT NULL,
-  `CoordenadaE` varchar(30) NOT NULL,
+  `CoordenadaN` decimal(30,0) NOT NULL,
+  `CoordenadaE` decimal(30,0) NOT NULL,
   `IdTipoInmueble` int(11) NOT NULL,
   `TipoUso` varchar(10) NOT NULL,
   `CantidadAmbientes` int(11) NOT NULL,
   `PrecioInmueble` decimal(10,0) NOT NULL,
   `Disponible` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `inmueble`
+--
+
+INSERT INTO `inmueble` (`Id`, `IdPropietario`, `Direccion`, `CoordenadaN`, `CoordenadaE`, `IdTipoInmueble`, `TipoUso`, `CantidadAmbientes`, `PrecioInmueble`, `Disponible`) VALUES
+(5, 5, 'Pederne', '3030', '40', 2, 'ee', 3, '33', 1),
+(7, 9, 'Pringles', '9999999', '99999999', 1, 'Casa', 3, '444444', 1);
 
 -- --------------------------------------------------------
 
@@ -75,7 +91,8 @@ CREATE TABLE `inquilino` (
 --
 
 INSERT INTO `inquilino` (`Id`, `Nombre`, `Apellido`, `Dni`, `Telefono`, `Email`) VALUES
-(4, 'Ramiro', 'Gomez', '44444444', '44444444', 'ramiro@gmail.com');
+(6, 'Laura', 'Camargo', '6666666', '6666666', 'laura@gmail.com'),
+(7, 'Sofia', 'Lucero', '8888888', '8888888', 'sofia@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -110,7 +127,9 @@ CREATE TABLE `propietario` (
 --
 
 INSERT INTO `propietario` (`Id`, `Nombre`, `Apellido`, `Dni`, `Telefono`, `Email`) VALUES
-(5, 'Juan Ignacio', 'Lopez', '2222222', '2222222', 'juan@gmail.com');
+(5, 'Juan Ignacio', 'Lopez', '2222222', '2222222', 'juan@gmail.com'),
+(8, 'Ramiro', 'Fernandez', '8989898', '9898989', 'ramiro@gmail.com'),
+(9, 'Gerardo', 'Perez', '2222222', '2222222', 'gerardo@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -122,6 +141,14 @@ CREATE TABLE `tipoinmueble` (
   `Id` int(11) NOT NULL,
   `Descripcion` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tipoinmueble`
+--
+
+INSERT INTO `tipoinmueble` (`Id`, `Descripcion`) VALUES
+(1, 'Casa'),
+(2, 'Local');
 
 -- --------------------------------------------------------
 
@@ -197,19 +224,19 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `contrato`
 --
 ALTER TABLE `contrato`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `inmueble`
 --
 ALTER TABLE `inmueble`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `inquilino`
 --
 ALTER TABLE `inquilino`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `pago`
@@ -221,13 +248,13 @@ ALTER TABLE `pago`
 -- AUTO_INCREMENT de la tabla `propietario`
 --
 ALTER TABLE `propietario`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `tipoinmueble`
 --
 ALTER TABLE `tipoinmueble`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
